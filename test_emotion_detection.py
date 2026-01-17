d9b1bfa20d471fdf36eb5b12b8069fb38f6cac30
PNapat <PNapat>
2026-01-17 07:36:29 -0500

    temporary save

---
+++ test_emotion_detection.py
@@ -0,0 +1,31 @@
+import unittest
+from EmotionDetection.emotion_detection import emotion_detector
+
+
+class TestEmotionDetection(unittest.TestCase):
+
+    def test_emotion_detector(self):
+        # Each call is expected to return a dictionary that includes "dominant_emotion".
+        self.assertEqual(
+            emotion_detector("I am glad this happened")["dominant_emotion"],
+            "joy"
+        )
+        self.assertEqual(
+            emotion_detector("I am really mad about this")["dominant_emotion"],
+            "anger"
+        )
+        self.assertEqual(
+            emotion_detector("I feel disgusted just hearing about this")["dominant_emotion"],
+            "disgust"
+        )
+        self.assertEqual(
+            emotion_detector("I am so sad about this")["dominant_emotion"],
+            "sadness"
+        )
+        self.assertEqual(
+            emotion_detector("I am really afraid that this will happen")["dominant_emotion"],
+            "fear"
+        )
+
+# Run all the test cases defined in the module when the script is executed.
+unittest.main()
